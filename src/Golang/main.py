5e08c0f4dd1460c3fb03e57b054ef28eb0f9c35f
Lin Wenqiu <1504803016@qq.com>
2024-08-31 18:43:37 +0800

    实现了go直接转换到json(AST树)

--- src/Golang/main.py
+++ src/Golang/main.py
@@ -1,6 +1,6 @@
 import subprocess
 import json
-
+import GoASTConverter
 
 def get_go_ast(file_path):
     result = subprocess.run(['GolangTool/AstGenerator.exe', file_path], capture_output=True, text=True)
@@ -9,5 +9,10 @@ def get_go_ast(file_path):
 def get_go_ssa(file_path):
     result = subprocess.run(['GolangTool/SSAGenerator.exe', '-build=F',file_path], capture_output=True, text=True)
 
-get_go_ast('test.go')
+def get_json_from_go_ast(file_path):
+    get_go_ast(file_path)
+    ast_path=''.join(file_path.split('.')[:-1])+".ast"
+    GoASTConverter.convert_ast_to_json(ast_path)
+
+get_json_from_go_ast('test.go')
 get_go_ssa('test.go')
