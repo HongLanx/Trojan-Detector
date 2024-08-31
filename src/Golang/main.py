dcc8c5513bc3bfefb9da71f89b9ee1f7ccf5e55e
Lin Wenqiu <1504803016@qq.com>
2024-08-31 21:33:07 +0800

    提供了批处理go转换JSON功能

--- src/Golang/main.py
+++ src/Golang/main.py
@@ -1,7 +1,8 @@
 import subprocess
-import json
+import tkinter as tk
+from tkinter import filedialog
 import GoASTConverter
-
+import os
 
 # 输入一个go文件，在其目录输出一个go自带ast库生成的ast文本文件
 def get_go_ast(file_path):
@@ -19,6 +20,22 @@ def get_json_from_go_ast(file_path):
     ast_path = ''.join(file_path.split('.')[:-1]) + ".ast"
     GoASTConverter.convert_ast_to_json(ast_path)
 
+def get_json_from_folder(directory):
+    for root, _, files in os.walk(directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            if file_path.split('.')[-1]=="go":
+                get_json_from_go_ast(file_path)
 
-get_json_from_go_ast('test.go')
-get_go_ssa('test.go')
+def select_directory():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main tkinter window
+    folder_selected = filedialog.askdirectory()
+    if folder_selected:
+        get_json_from_folder(folder_selected)
+        print(f"Processing completed for folder: {folder_selected}")
+    else:
+        print("No folder selected.")
+
+select_directory()
+# get_go_ssa('test.go')
