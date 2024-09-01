b76f73d1de7d6fe189f77cb799e63bbda7073b06
Lin Wenqiu <1504803016@qq.com>
2024-09-01 22:48:44 +0800

    实现了字符串的模糊匹配

--- src/Golang/get_score.py
+++ src/Golang/get_score.py
@@ -1,11 +1,11 @@
 import json
-
+import re
 # 参数：提取后的项目信息（JSON格式），病毒木马模式类型
 # 返回匹配得到的恶意评分
 # 初始恶意评分为0
 # 第一次匹配要求完全匹配，若匹配成功则加上对应的分数，对于Imports和string，多次匹配成功只记作匹配成功1次
 # 由于JSON内的calls可能出现多次，对于calls和FunctionCalls的匹配，如果是第一次匹配，则总恶意评分加上对应的分数，后面每多匹配一次，在总恶意评分只加1
-
+# 对于字符串，进行两轮匹配，分别是精确匹配和模糊匹配，模糊匹配的加分值比精确匹配低5分
 def get_score_from_info(json_data, pattern):
     # 解析JSON数据
     project_info = json.loads(json_data)
@@ -19,8 +19,8 @@ def get_score_from_info(json_data, pattern):
     # 匹配 Imports
     for imp in project_info['imports']:
         if imp in pattern['Imports']:
-            print(f"发现疑似僵尸网络特征Import：{imp}")
             if imp not in matched_imports:
+                print(f"发现疑似僵尸网络特征Import：{imp}")
                 malicious_score += pattern['Imports'][imp]
                 matched_imports.add(imp)
 
@@ -36,13 +36,28 @@ def get_score_from_info(json_data, pattern):
                 malicious_score += 1
             matched_calls.add(func_name)
 
-    print("正在匹配字符串...")
+    print("正在精确匹配字符串...")
     # 匹配 Strings
     for string in project_info['string']:
         if string in pattern['Strings']:
-            print(f"发现疑似僵尸网络特征字符串：{string}")
             if string not in matched_strings:
+                print(f"精确匹配：发现疑似僵尸网络特征字符串：{string}")
                 malicious_score += pattern['Strings'][string]
                 matched_strings.add(string)
+
+    print("正在模糊匹配字符串...")
+    for found_string in project_info['string']:
+        # 检查每个模式是否在发现的字符串中
+        for string, score in pattern['Strings'].items():
+            # 使用正则表达式进行模糊匹配，例如匹配 '*ddos*' 等
+            if re.search(r'.*{}.*'.format(re.escape(string)), found_string, re.IGNORECASE):
+                # 确保每个模式只被计算一次
+                if string not in matched_strings:
+                    print(f"模糊匹配：发现疑似僵尸网络特征字符串：{found_string}")
+                    # 如果匹配成功，并且原评分大于5，则加上原评分-5
+                    if score > 5:
+                        malicious_score += (score - 5)
+                    matched_strings.add(string)
+                    break  # 匹配到一个字符串后，不再尝试其他模式
 
     return malicious_score
